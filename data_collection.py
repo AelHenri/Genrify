35eb13a641e44a926c49b626a4eacda273fde2f4
Henri <t.henri.al@gmail.com>
2016-10-30 00:09:13 -0500

    data collection

--- data_collection.py
+++ data_collection.py
@@ -1,9 +1,11 @@
 import spotipy
 import spotipy.util as util
 import csv
+import time
 
 NUM_TRACKS = 100
-GENRES = ['jazz','blues','country','classical','french','alternative','rock','pop','electro','r-n-b','hip-hop','soul','techno','hard-rock','reggae','folk','indie','punk','heavy-metal','psych-rock',]
+GENRES = ['jazz','blues','country','classical','french','alternative','rock','pop','electro','r-n-b','hip-hop','soul','techno','hard-rock','reggae','folk','indie','punk-rock','heavy-metal','psych-rock']
+COLUMNS = ['artist','track','danceability','energy','key','loudness','speechiness','acousticness','instrumentalness','liveness','valence','tempo','duration_ms','time_signature']
 
 SPOTIPY_CLIENT_ID='f380296eefe34641ba6601f235f24c85'
 SPOTIPY_CLIENT_SECRET='c13a31dcc76f43c792f6b7cd266450c2'
@@ -17,7 +19,7 @@ sp = spotipy.Spotify(auth=token)
 def writeToCSV(tracks):
 	keys = tracks[0].keys()
 	with open('music_collection.csv', 'w') as csvfile:
-		writer = csv.DictWriter(csvfile, keys)
+		writer = csv.DictWriter(csvfile, fieldnames = COLUMNS+GENRES)
 	
 		writer.writeheader()
 		writer.writerows(tracks)
@@ -26,8 +28,14 @@ def collectTracks():
 	tracks = []
 	for genre in GENRES:
 		unprocessedTracks = sp.recommendations(seed_genres=genre, limit=NUM_TRACKS)
-		for i in range(NUM_TRACKS):
+		n = NUM_TRACKS
+		length = len(unprocessedTracks['tracks'])
+		if (length != NUM_TRACKS):
+			print("Got less than " + str(NUM_TRACKS) + " tracks.")
+			n = length
+		for i in range(n):
 			tracks.append(buildTrack(unprocessedTracks['tracks'][i], genre))
+		time.sleep(1)
 	return tracks
 
 def buildTrack(unprocessedTrack, genre):
@@ -48,7 +56,8 @@ def buildTrack(unprocessedTrack, genre):
 	processedTrack['tempo'] = features['tempo']
 	processedTrack['duration_ms'] = features['duration_ms']
 	processedTrack['time_signature'] = features['time_signature']
-	processedTrack['genre'] = genre
+	for g in GENRES:
+		processedTrack[g] = 1 if g == genre else 0
 
 	return processedTrack
 
